cd9f0617bcbd3ceb7f4c28be67a5efd53dd6c313
Madhu Sudhan <m02sudhan@gmail.com>
2022-11-28 19:13:33 +0530

    Added : Switch statement

---
+++ ifelseifelse.js
@@ -0,0 +1,58 @@
+// 1. Read a single digit number and write the number in word
+
+var digit = 2;
+if(digit==0){
+console.log('zero');
+}
+else if(digit==1){
+    console.log('one');
+}
+else if(digit==3){
+    console.log('three');
+}
+else if(digit==2){
+    console.log('two');
+}
+else if(digit==4){
+    console.log('four');
+}
+else if(digit==5){
+    console.log('five');
+}
+else {
+    console.log('enter valid number');
+}
+
+// console.log('five');
+
+
+
+
+
+// 2. Read a Number and Display the week day (Sunday, Monday,...)
+
+var day = "monday";
+switch(day){
+    case "monday" :
+        console.log("its monday");
+        break;
+        
+    case "tuesday" :
+        console.log("its tuesday");
+        break;
+
+    default :
+    console.log("enter valid day")
+}
+
+// let day =
+
+
+
+// 3. Read a Number 1, 10, 100, 1000, etc and display unit, ten, hundred,...
+// 4. Enter 3 Numbers do following arithmetic operation and find the one that
+
+// is maximum and minimum
+// 1. a + b * c 3. c + a / b
+// 2. a % b + c 4. a * b + c
+
